48d65ec7edd69440a24b6a6d042c3253fbf6abe9
saikr <saikr@DESKTOP-2TPVV0K>
2016-12-28 15:46:25 -0500

    update

--- Hackerrank/saveprincess.cs
+++ Hackerrank/saveprincess.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,19 +28,19 @@ namespace Hackerrank
             }
         }
 
-        //static void Main(String[] args)
-        //{
-        //    int m;
-        //    m = int.Parse(Console.ReadLine());
+        static void Main(String[] args)
+        {
+            int m;
+            m = int.Parse(Console.ReadLine());
 
-        //    char[][] grid = new char[m][];
-        //    for(int i=0;i<m; i++)
-        //    {
-        //        grid[i] = Console.ReadLine().ToCharArray();
-        //    }
+            char[][] grid = new char[m][];
+            for (int i = 0; i < m; i++)
+            {
+                grid[i] = Console.ReadLine().ToCharArray();
+            }
 
-        //    displayPathtoPrincess(m, grid);
-        //}
+            displayPathtoPrincess(m, grid);
+        }
 
         static int[] findPosition(char a, char[][] grid)
         {
@@ -56,6 +57,66 @@ namespace Hackerrank
                 }
             }
             return coords;
+        }
+
+        static string[] romanizer(int[] numbers)
+        {
+            String[] result = new string[numbers.Length];
+            Dictionary<int, String> dictionary = new Dictionary<int, string>();
+            populateDictionary(dictionary);
+            for(int i=0; i<numbers.Length; i++)
+            {
+                int number = numbers[i];
+                string res = "";
+                if (number == 1000)
+                    result[i] = dictionary[number];
+                else
+                {
+                    for (int count = 1; count < 4; count++)
+                    {
+                        int digit = number % ((int)Math.Pow(10,count));
+                        if (digit > 0)
+                        {
+                            res = dictionary[digit] + res;
+                            number = number - digit;
+                        }
+                    }
+                    result[i] = res;
+                }
+            }
+            return result;
+        }
+
+        static void populateDictionary(Dictionary<int, String> dictionary)
+        {
+            dictionary.Add(1, "I");
+            dictionary.Add(2, "II");
+            dictionary.Add(3, "III");
+            dictionary.Add(4, "IV");
+            dictionary.Add(5, "V");
+            dictionary.Add(6, "VI");
+            dictionary.Add(7, "VII");
+            dictionary.Add(8, "VIII");
+            dictionary.Add(9, "IX");
+            dictionary.Add(10, "X");
+            dictionary.Add(20, "XX");
+            dictionary.Add(30, "XXX");
+            dictionary.Add(40, "XL");
+            dictionary.Add(50, "L");
+            dictionary.Add(60, "LX");
+            dictionary.Add(70, "LXX");
+            dictionary.Add(80, "LXXX");
+            dictionary.Add(90, "XC");
+            dictionary.Add(100, "C");
+            dictionary.Add(200, "CC");
+            dictionary.Add(300, "CCC");
+            dictionary.Add(400, "CD");
+            dictionary.Add(500, "D");
+            dictionary.Add(600, "DC");
+            dictionary.Add(700, "DCC");
+            dictionary.Add(800, "DCCC");
+            dictionary.Add(900, "CM");
+            dictionary.Add(1000, "M");
         }
     }
 }
